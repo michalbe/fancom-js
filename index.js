bae457df3de1800d929eee8f320c1c4e176f5792
Michal Budzynski <michal@virtualdesign.pl>
2014-08-18 01:19:31 +0200

    [FANCOM-JS] - :school: lint fixes

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var sre = require('simple-random-emoji');
 
 gi(['author', 'branch', 'repository', 'name'], function(err, data) {
   var currentRepoInfo = data;
-  console.log(currentRepoInfo);
+  console.log(currentRepoInfo, sre());
 });
