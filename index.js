ade3d62625024c6062b5b39c4377023568e88d4d
Michal Budzynski <michal@virtualdesign.pl>
2014-08-18 10:56:03 -0500

    [FANCOM-JS] - :name_badge: fancom.json additional comment + new dependencies

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ var gi = require('git-info');
 var sre = require('simple-random-emoji');
 
 gi(['author', 'branch', 'repository', 'name'], function(err, data) {
-  var currentRepoInfo = data;
-  console.log(currentRepoInfo, sre());
+  var config = {
+    "reponame"        : "reponame",
+    "pushtotemp"      : 1,
+    "comparewith"     : "branch to compare",
+    "devsuffix"       : "temp",
+    "emoji"           : 1,
+    "addall"          : 1,
+    "prefix"          : "Shit's on fire yo",
+    "branchincommit"  : 1,
+    "noprefix"        : 1,
+    "opencompare"     : 1,
+    "author"          : "authorname"
+  }
 });
