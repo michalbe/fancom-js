459a9b0d300cf720eab82341c367f9b9d56917d7
Michal Budzynski <michal@virtualdesign.pl>
2014-08-18 10:56:40 -0500

    [FANCOM-JS] - :syringe: lint fixes

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ var sre = require('simple-random-emoji');
 
 gi(['author', 'branch', 'repository', 'name'], function(err, data) {
   var config = {
-    "reponame"        : "reponame",
-    "pushtotemp"      : 1,
-    "comparewith"     : "branch to compare",
-    "devsuffix"       : "temp",
-    "emoji"           : 1,
-    "addall"          : 1,
-    "prefix"          : "Shit's on fire yo",
-    "branchincommit"  : 1,
-    "noprefix"        : 1,
-    "opencompare"     : 1,
-    "author"          : "authorname"
-  }
+    'reponame'        : 'reponame',
+    'pushtotemp'      : 1,
+    'comparewith'     : 'branch to compare',
+    'devsuffix'       : 'temp',
+    'emoji'           : 1,
+    'addall'          : 1,
+    'prefix'          : 'Shit\'s on fire yo',
+    'branchincommit'  : 1,
+    'noprefix'        : 1,
+    'opencompare'     : 1,
+    'author'          : 'authorname'
+  };
+  console.log(config.prefix, sre);
 });
